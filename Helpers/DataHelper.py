ecb4eb93df46cb7435dcf87e50ddbf94de8b3160
ADS\srivasak <akash.srivastava@autodesk.com>
2018-11-27 18:59:45 +0800

    added create user test

--- Helpers/DataHelper.py
+++ Helpers/DataHelper.py
@@ -11,3 +11,15 @@ class DataHelper():
         print(json_data)
         return json_data
 
+    def build_data_for_create_user_post(self, user_email, user_password, first_name, last_name):
+        json_helper = JsonHelper()
+        file_path = r"../Resources/CreateUserModel.json"
+        dictionary_data = json_helper.load_json_file(file_path)
+        dictionary_data["UserEmail"] = user_email
+        dictionary_data["UserPassword"] = user_password
+        dictionary_data["FirstName"] = first_name
+        dictionary_data["LastName"] = last_name
+        json_data = json_helper.covert_into_json(dictionary_data)
+        print(json_data)
+        return json_data
+
--- Tests/UserLoadTest.py
+++ Tests/UserLoadTest.py
@@ -1,5 +1,6 @@
 import sys
 import random
+import uuid
 sys.path.insert(0,r"..\Helpers")
 from CsvHelper import CsvHelper
 from DataHelper import DataHelper
@@ -30,8 +31,20 @@ class UserBehavior(TaskSet):
         print(data)
         response = self.client.post("/api/tokens", data = data, headers= headers, verify = False)
         actual_status_code = response.status_code
-        assert actual_status_code == 200, "correct status for token should be received expected 200  found = {0}".format(actual_status_code)
-         
+        assert actual_status_code == 200, "correct status for token should be received expected 200  found = {0} \n userEmail = {1} \n password= {2}".format(actual_status_code, email, password)
+
+    def create_user(self):
+        first_name = "abc" + str(uuid.uuid4())[:8]
+        email = first_name + "@yopmail.com"
+        password = "Password1"
+        last_name = "loadTestUser"
+        data = data_helper.build_data_for_create_user_post(email, password, first_name, last_name)
+        headers = {"content-type": 'application/json'}
+        print(data)
+        response = self.client.post("/api/users", data = data, headers= headers, verify = False)
+        actual_status_code = response.status_code
+        assert actual_status_code == 201, "correct status for token should be received expected 201  found = {0}".format(actual_status_code)
+
 class WebsiteUser(HttpLocust):
     """
         defines the user task behavior
